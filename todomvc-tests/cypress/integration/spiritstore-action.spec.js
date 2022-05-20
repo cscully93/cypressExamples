0ffa17ce7279b1f22cdacea8e16f2b521e64feaf
cscully93 <60076315+cscully93@users.noreply.github.com>
2022-05-20 16:19:11 +0100

    Add files via upload

--- todomvc-tests/cypress/integration/spiritstore-action.spec.js
+++ todomvc-tests/cypress/integration/spiritstore-action.spec.js
@@ -1,29 +1,32 @@
 ///<reference types="cypress"/>
 
-describe('SpiritStore actions', () => {
+import {
+  startFromHomepage,
+  navigateToMostRecentGig,
+  buyTickets,
+  navigateToTab,
+  scrollToEmail,
+  typeIntoEmailField,
+} from "../page-objects/spiritStoreHomepage";
 
-    beforeEach(() => {
-        cy.visit('https://www.spiritstore.ie/')
-    })
+describe("SpiritStore actions", () => {
+  beforeEach(() => {
+    startFromHomepage();
+  });
 
-    it('Navigate to most recent announced gig', () => {
-        cy.get(':nth-child(1) > .listings-button-box > .event-button').click()
-        cy.get('.event-header > h1').should('be.visible')
-        
-        //Un-comment lines when tickets for gig are available for purchase
+  it("Navigate to most recent announced gig", () => {
+    navigateToMostRecentGig();
+    // Un-comment lines when tickets for gig are available for purchase
+    // buyTickets('3')
+  });
 
-        // cy.get('[id=quantity]').select('3')
-        // cy.get('.paypal_btn').click()
-    })
+  it('Navigate to "About" page and scroll down page', () => {
+    navigateToTab("About");
+    scrollToEmail();
+  });
 
-    it('Navigate to "About" page and scroll down page', () => {
-        cy.get(':nth-child(5) > a').click()
-        cy.get('#mce-EMAIL').scrollIntoView();
-    })
-
-    it('Navigate to "Bar" page and input text into Newsletter email field', () => {
-        cy.get('.site-nav > ul > :nth-child(3) > a').click()
-        cy.get('#mce-EMAIL').type("This is a cypress test")
-    })
-
-})
+  it('Navigate to "Bar" page and input text into Newsletter email field', () => {
+    navigateToTab("Bar");
+    typeIntoEmailField();
+  });
+});
