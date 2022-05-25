d336845a702006a1bc59e5729962483de3aca60b
cscully93 <60076315+cscully93@users.noreply.github.com>
2022-05-25 11:38:09 +0100

    Add files via upload

--- todomvc-tests/cypress/page-objects/spiritStoreHomepage.js
+++ todomvc-tests/cypress/page-objects/spiritStoreHomepage.js
@@ -1,17 +1,31 @@
 /// <reference types="cypress" />
 
+const mostRecentGigButton =":nth-child(1) > .listings-button-box > .event-button";
+const eventHeader = ".event-header > h1";
+const ticketQuantitySelector = "[id=quantity]";
+const paypalBtn = ".paypal_btn";
+const emailField = "#mce-EMAIL";
+
 export function startFromHomepage() {
-  cy.visit(Cypress.env("baseUrl"));
+  cy.visit("/");
 }
-
 export function navigateToMostRecentGig() {
-  cy.get(":nth-child(1) > .listings-button-box > .event-button").click();
-  cy.get(".event-header > h1").should("be.visible");
+  cy.get(mostRecentGigButton).click();
+  cy.get(eventHeader).should("be.visible");
 }
 
-export function buyTickets(numTickets) {
-  cy.get("[id=quantity]").select(numTickets);
-  cy.get(".paypal_btn").click();
+export function selectTickets(numTickets) {
+  cy.get(ticketQuantitySelector).select(numTickets);
+}
+
+export function clickPayPalBtn() {
+  cy.get(paypalBtn).click();
+}
+
+export function verifyPayPalSite() {
+  cy.fixture("data.json").then((data) => {
+    cy.url().should("include", data.text.payPalUrl);
+  });
 }
 
 export function navigateToTab(tabName) {
@@ -19,11 +33,11 @@ export function navigateToTab(tabName) {
 }
 
 export function scrollToEmail() {
-  cy.get("#mce-EMAIL").scrollIntoView();
+  cy.get(emailField).scrollIntoView();
 }
 
 export function typeIntoEmailField() {
   cy.fixture("data.json").then((data) => {
-  cy.get("#mce-EMAIL").type(data.text.emailField);
+    cy.get(emailField).type(data.text.emailFieldInput);
   });
 }
