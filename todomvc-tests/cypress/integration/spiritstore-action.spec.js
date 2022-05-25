b422418046ce390892af241d8d842ebd0762b5c4
cscully93 <60076315+cscully93@users.noreply.github.com>
2022-05-25 11:37:18 +0100

    Add files via upload

--- todomvc-tests/cypress/integration/spiritstore-action.spec.js
+++ todomvc-tests/cypress/integration/spiritstore-action.spec.js
@@ -1,32 +1,27 @@
 ///<reference types="cypress"/>
 
-import {
-  startFromHomepage,
-  navigateToMostRecentGig,
-  buyTickets,
-  navigateToTab,
-  scrollToEmail,
-  typeIntoEmailField,
-} from "../page-objects/spiritStoreHomepage";
+import {clickPayPalBtn,startFromHomepage,navigateToMostRecentGig,verifyPayPalSite,navigateToTab,scrollToEmail,typeIntoEmailField,selectTickets} from "../page-objects/spiritStoreHomepage";
 
 describe("SpiritStore actions", () => {
+
   beforeEach(() => {
     startFromHomepage();
   });
-
   it("Navigate to most recent announced gig", () => {
     navigateToMostRecentGig();
     // Un-comment lines when tickets for gig are available for purchase
-    // buyTickets('3')
+    selectTickets("3");
+    clickPayPalBtn();
+    clickPayPalBtn();
+    verifyPayPalSite();
   });
-
   it('Navigate to "About" page and scroll down page', () => {
     navigateToTab("About");
     scrollToEmail();
   });
-
   it('Navigate to "Bar" page and input text into Newsletter email field', () => {
     navigateToTab("Bar");
     typeIntoEmailField();
   });
+
 });
